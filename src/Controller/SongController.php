01ee7e92ef2dfb70750884df4461901574de3c65
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2023-10-26 22:53:45 -0400

    Chapter 13 - Regex in routes

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -9,9 +9,10 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id}')]
-    public function getSong($id): Response
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
+    public function getSong(int $id): Response
     {
+
         // TODO query the database
         $song = [
             'id' => $id,
