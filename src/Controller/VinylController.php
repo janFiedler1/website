a1d4c8fb32814ad1a5b2d354d63e97b5911b8ebc
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2023-10-26 23:35:09 -0400

    Chapter 15 - Twig Service

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,11 +3,12 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
 use function Symfony\Component\String\u;
 class VinylController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage(): Response
+    public function homepage(Environment $twig): Response
     {
         $tracks = [
             ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
@@ -20,12 +21,18 @@ class VinylController extends AbstractController
         ];
         //dd($tracks);
         //dump($tracks);
-            
+        /*    
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks,
         ]);
-        
+        */
+        $html = $twig->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
+        //dd($html);
+        return new Response($html);
 
     }
     #[Route('/browse/{slug}', name: 'app_browse')]  //{coolMusicGenre}
